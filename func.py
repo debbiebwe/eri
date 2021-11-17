a79b2e8a054cf69dc3dff01be8192ef1d62d232a
Foris Cai <foris323@gmail.com>
2021-11-17 22:01:37 +0800

    init poker game

--- func.py
+++ func.py
@@ -1,6 +1,28 @@
-def func1():
-    return 1
+"""
+10 = T
+紅心 = H
+方塊 = D
+黑桃 = S
+梅花 = C
+
+紅心10 = TH
+
+雙方不同牌型
+    player1: 對子
+    plarer2: 葫蘆
+    result: Player1 win
+
+雙方相同牌型
+    player1:  55441
+    player2:  55332
+    result: Player1 win the game with two pair 44
+"""
 
 
-def test1():
-    print("bwe")
+class PokerGame():
+
+    def __init__(self, player1, player2):
+        pass
+
+    def result(self):
+        pass
