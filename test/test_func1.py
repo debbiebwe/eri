4598005cdc348ab79ce8661d9af7e08983c24f88
debbiebwe <32321780+debbiebwe@users.noreply.github.com>
2021-11-18 22:34:57 +0900

    test: add nothing, onepair

--- test/test_func1.py
+++ test/test_func1.py
@@ -3,8 +3,27 @@ from func import PokerGame
 
 def test_nothing_winner():
     game = PokerGame("1C 2H 6S 9S TD", "2C 4H 5S 8S 9D")
-    assert game.result() == "Player1 win the game"
+    assert game.result() == "Player1 win the game with nothing 1C"
 
+
+def test_nothing_winner_jqk_1():
+    game = PokerGame("QC 2H 6S 9S TD", "2C 4H JS 8S 9D")
+    assert game.result() == "Player1 win the game with nothing QC"
+
+
+def test_nothing_winner_jqk_2():
+    game = PokerGame("JC 2H 6S 9S TD", "2C 4H KS 8S 9D")
+    assert game.result() == "Player2 win the game with nothing KS"
+
+
+def test_nothing_no_winner():
+    game = PokerGame("1C 2H 6S 8S KD", "2C 1H KS 8D 6D")
+    assert game.result() == "No winner"
+
+
+def test_onepair_nothing_1():
+    game = PokerGame("1C 1H 3S 6S KD", "2C 1H KS 8D 6D")
+    assert game.result() == "Player1 win the game"
 
 # 10 = T
 # 紅心 = H
